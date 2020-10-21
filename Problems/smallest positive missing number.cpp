6b7d6b3b04262b7a91ecfd227350b0d0a7b7b063
Ankit Phondani <ankitphondani52@gmail.com>
2020-10-21 10:57:17 +0530

    Rearrange an array with no extra space

--- Problems/smallest positive missing number.cpp
+++ Problems/smallest positive missing number.cpp
@@ -1,5 +1,7 @@
 // Medium Accuracy: 38.31% Submissions: 8643 Points = 4
 // You are given an array arr[] of N integers including 0. The task is to find the smallest positive number missing from the array.
+// Time Complexity: O(N)
+// space: O(N), @TODO: expected O(1)
 
 #include <bits/stdc++.h>
 using namespace std;
